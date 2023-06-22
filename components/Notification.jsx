cceff1310f659e8b6103255261a3e1ff4ba146b6
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2023-06-23 01:45:17 +0530

    updated notification

--- components/Notification.jsx
+++ components/Notification.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Notification = () => {
   return (
-    <div className=' w-[100%] md:w-[80%] sticky top-14 z-[100] mx-auto mt-5 bg-red-500 text-2xl text-white'><marquee width="100%"  direction="left" height="30px">
-        Please make sure to unblock api call by allowing insecure content in settings.
+    <div className=' w-[100%] md:w-[80%] sticky top-14 z-[100] mx-auto mt-5 bg-red-400 text-2xl text-white'><marquee width="100%"  direction="left" height="30px">
+        Please make sure to unblock api call by allowing insecure content in settings.For more info  <a className='text-blue-500 underline' target='blank' href="https://github.com/anurag-327/PolymathAIProject/blob/main/README.md">Visit this</a>
     </marquee></div>
   )
 }
